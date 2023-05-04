9a5b5a8819ab67e34349eb8935d87fa40fe5c94d
Manuel2u <edodoo011@st.ug.edu.gh>
2023-05-04 01:03:25 +0000

    finished app

--- lib/main.dart
+++ lib/main.dart
@@ -11,35 +11,67 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blue[600],
         body: SafeArea(
-            child: Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Container(
-              color: Colors.red,
-              width: 100.0,
-            ),
-            Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    color: Colors.yellow,
-                    width: 100.0,
-                    height: 100.0,
+            child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage("images/profile.png"),
+              ),
+              const Text(
+                "Emmanuel Dodoo",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40.0,
+                    fontFamily: "Pacifico"),
+              ),
+              Text(
+                "FLUTTER DEVELOPER",
+                style: TextStyle(
+                    color: Colors.blue[100],
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5,
+                    fontSize: 20.0,
+                    fontFamily: "SourceSansPro"),
+              ),
+              SizedBox(
+                  width: 150.0,
+                  height: 20.0,
+                  child: Divider(
+                    color: Colors.blue.shade100,
+                  )),
+              const Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(color: Colors.blue, Icons.phone),
+                    title: Text(
+                      "+233 2414 89576",
+                      style: TextStyle(
+                          fontFamily: "SourceSansPro",
+                          fontSize: 20.0,
+                          color: Colors.blue),
+                    ),
+                  )),
+              const Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(color: Colors.blue, Icons.email),
+                  title: Text(
+                    "dodooemma123@gmail.com",
+                    style: TextStyle(
+                        fontFamily: "SourceSansPro",
+                        fontSize: 20.0,
+                        color: Colors.blue),
                   ),
-                  Container(
-                    color: Colors.green,
-                    width: 100.0,
-                    height: 100.0,
-                  )
-                ]),
-            Container(
-              color: Colors.blue,
-              width: 100.0,
-            ),
-          ],
+                ),
+              )
+            ],
+          ),
         )),
       ),
     );
